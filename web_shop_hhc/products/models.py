5c1a7c0dc30768f4f228de91191bfbb608334e39
AnatolySneg <AnatolySneg@gmail.com>
2023-02-10 00:57:17 +0200

    Start creating db models

--- web_shop_hhc/products/models.py
+++ web_shop_hhc/products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(max_length=1000)
+    price = models.FloatField()
+    is_sale = models.BooleanField(default=False)
+    discount = models.IntegerField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    available_quantity = models.IntegerField(default=0, max_length=6)
+
+    def __str__(self):
+        return self.title
+
+# ADD USERS, COMMENTS, purchase history, product rating
