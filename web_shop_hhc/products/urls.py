d1b8f926ed44b72d47d7b8aed8ba3c3c473020af
AnatolySneg <AnatolySneg@gmail.com>
2023-03-27 00:48:17 +0300

    made urls in homepage, updated urls.py and null function in vies.py

--- web_shop_hhc/products/urls.py
+++ web_shop_hhc/products/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import home_page, product_list
+from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', product_list),
+    path('about_us/', contacts_page),
+    path('login/', login_page),
+    path('logout/', login_page),
+    path('user/', user_page),
+    path('bucket/', bucket),
 ]
 
 if settings.DEBUG:
--- web_shop_hhc/products/views.py
+++ web_shop_hhc/products/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+
+
 # Create your views here.
-
-
-def home_page(request):
-    print('home-page from console')
-    return HttpResponse('This is Home-page')
 
 
 def product_list(request):
@@ -15,3 +12,28 @@ def product_list(request):
         'products': products,
     }
                   )
+
+
+def contacts_page(request):
+    print('contacts_page from console')
+    return HttpResponse('This is Contacts_page')
+
+
+def login_page(request):
+    print('login_page from console')
+    return HttpResponse('This is login_page')
+
+
+def logout(request):
+    print('logout_process from console')
+    return HttpResponse('This is logout_process')
+
+
+def user_page(request):
+    print('user_page from console')
+    return HttpResponse('This is user_page')
+
+
+def bucket(request):
+    print('Bucket_page from console')
+    return HttpResponse('This is Bucket_page')
