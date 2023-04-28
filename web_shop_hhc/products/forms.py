320746cc39fd5fa013176cabf990ca4512c7fd2c
AnatolySneg <AnatolySneg@gmail.com>
2023-04-29 01:05:07 +0300

    working with ordering forms

--- web_shop_hhc/products/forms.py
+++ web_shop_hhc/products/forms.py
@@ -1,4 +1,4 @@
-from django import forms
+from django.forms import PasswordInput, ModelForm, CharField, EmailField
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
@@ -12,8 +12,8 @@ class UserSignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2', ]
-        widgets = {'password1': forms.PasswordInput(),
-                   'password2': forms.PasswordInput(),
+        widgets = {'password1': PasswordInput(),
+                   'password2': PasswordInput(),
                    }
 
     def clean(self):
@@ -27,14 +27,104 @@ class UserSignupForm(UserCreationForm):
             raise ValidationError("Email exist")
 
 
-class CustomerSignupForm(forms.ModelForm):
+class CustomerSignupForm(ModelForm):
     class Meta:
         model = Customer
         fields = ['phone_number', 'birth_date']
 
 
-class OrderFirstCreationForm(forms.ModelForm):
+class OrderFirstCreationForm(ModelForm):
+    first_name = CharField(max_length=100)
+    middle_name = CharField(max_length=100, blank=True, null=True)
+    last_name = CharField(max_length=100)
+    email = EmailField(blank=True, null=True)
+    delivery_option =CharField(choices=Order.DELIVERY_OPTIONS, default=Order.PICKUP, max_length=50)
     class Meta:
         model = Order
-        fields = ['first_name', 'middle_name', 'last_name', 'email', 'phone_number', 'delivery_option',
-                  'delivery_option']
+        fields = ['first_name', 'middle_name', 'last_name', 'email', 'phone_number', 'delivery_option']
+
+
+class OrderSecondPickupCreationForm(ModelForm):
+    ONLINE_PAYMENT = 'ONLINE_PAYMENT'
+    PAYMENT_BY_CARD_ON_RECEIPT = 'PAYMENT_BY_CARD'
+    PAYMENT_IN_CASH_ON_RECEIPT = 'PAYMENT_IN_CASH'
+
+    PAYMENT_OPTIONS = [
+        (ONLINE_PAYMENT, 'Online payment'),
+        (PAYMENT_BY_CARD_ON_RECEIPT, 'Payment by card upon receipt'),
+        (PAYMENT_IN_CASH_ON_RECEIPT, 'Cash payment'),
+    ]
+
+    payment_option =CharField(choices=PAYMENT_OPTIONS, max_length=50)
+    destination_region =CharField(max_length=100, blank=True, null=True)
+    destination_country =CharField(max_length=50, null=True)
+    destination_delivery_service =CharField(max_length=100, null=True)
+    destination_street =CharField(max_length=100, null=True)
+    destination_house = CharField(max_length=20, null=True)
+    destination_apartment = CharField(max_length=20, blank=True, null=True)
+
+
+    class Meta:
+        model = Order
+        fields = ['payment_option', 'destination_region', 'destination_country', 'destination_delivery_service', 'destination_street', 'destination_house',
+                  'destination_apartment']
+
+
+class OrderSecondCourierCreationForm(ModelForm):
+    ONLINE_PAYMENT = 'ONLINE_PAYMENT'
+    PAYMENT_BY_CARD_ON_RECEIPT = 'PAYMENT_BY_CARD'
+    PAYMENT_IN_CASH_ON_RECEIPT = 'PAYMENT_IN_CASH'
+    CASH_ON_DELIVERY = 'CASH_ON_DELIVERY'
+
+    PAYMENT_OPTIONS = [
+        (ONLINE_PAYMENT, 'Online payment'),
+        (PAYMENT_BY_CARD_ON_RECEIPT, 'Payment by card upon receipt'),
+        (PAYMENT_IN_CASH_ON_RECEIPT, 'Cash payment'),
+        (CASH_ON_DELIVERY, 'Cash on delivery'),
+    ]
+
+
+    payment_option =CharField(choices=Order.PAYMENT_OPTIONS, default=Order.PAYMENT_BY_CARD_ON_RECEIPT, max_length=50)
+    destination_region =CharField(max_length=100, blank=True, null=True)
+    destination_country =CharField(max_length=50, null=True)
+    destination_delivery_service =CharField(max_length=100, null=True)
+    destination_street =CharField(max_length=100, null=True)
+    destination_house = CharField(max_length=20, null=True)
+    destination_apartment = CharField(max_length=20, blank=True, null=True)
+
+
+    class Meta:
+        model = Order
+        fields = ['payment_option', 'destination_region', 'destination_country', 'destination_delivery_service', 'destination_street', 'destination_house',
+                  'destination_apartment']
+
+
+class OrderSecondDeliveryCreationForm(ModelForm):
+    ONLINE_PAYMENT = 'ONLINE_PAYMENT'
+    PAYMENT_BY_CARD_ON_RECEIPT = 'PAYMENT_BY_CARD'
+    PAYMENT_IN_CASH_ON_RECEIPT = 'PAYMENT_IN_CASH'
+    CASH_ON_DELIVERY = 'CASH_ON_DELIVERY'
+
+    PAYMENT_OPTIONS = [
+        (ONLINE_PAYMENT, 'Online payment'),
+        (PAYMENT_BY_CARD_ON_RECEIPT, 'Payment by card upon receipt'),
+        (PAYMENT_IN_CASH_ON_RECEIPT, 'Cash payment'),
+        (CASH_ON_DELIVERY, 'Cash on delivery'),
+    ]
+
+
+    payment_option =CharField(choices=Order.PAYMENT_OPTIONS, default=Order.PAYMENT_BY_CARD_ON_RECEIPT, max_length=50)
+    destination_region =CharField(max_length=100, blank=True, null=True)
+    destination_country =CharField(max_length=50, null=True)
+    destination_delivery_service =CharField(max_length=100, null=True)
+    destination_street =CharField(max_length=100, null=True)
+    destination_house = CharField(max_length=20, null=True)
+    destination_apartment = CharField(max_length=20, blank=True, null=True)
+
+
+    class Meta:
+        model = Order
+        fields = ['payment_option', 'destination_region', 'destination_country', 'destination_delivery_service', 'destination_street', 'destination_house',
+                  'destination_apartment']
+
+
